1197f29390ccc0eb2e79ee46dda6419cecffb350
Jacob de Vos <jacobdevos@hotmail.com>
2019-05-01 14:02:23 -0400

    playing with member vs user

--- DiscordBot.py
+++ DiscordBot.py
@@ -20,7 +20,7 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        print(type(message.author))
+        print("{}".format(type(message.author)))
         if type(message.author) == 'discord.member.Member':
             print("made it")
             await message.channel.send('Hello {}!'.format(message.author.display_name))
