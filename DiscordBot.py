a016c06ef54ebbc40ed0059f36b8a3f5a2191301
Jacob de Vos <jacobdevos@hotmail.com>
2019-05-01 16:12:58 -0400

    updating api call

--- DiscordBot.py
+++ DiscordBot.py
@@ -40,18 +40,18 @@ async def on_member_join(member):
 async def on_voice_state_update(member, before, after):
     if before.channel is None or (before.channel.name != "General" and after.channel.name == "General"):
         text_channel = member.guild.text_channels[0]
-        await  text_channel.send('Hello {}'.format(member.display_name))
+        await text_channel.send('Hello {}'.format(member.display_name))
         print(member.name)
         print(overwatch_dictionary)
         if member.name in overwatch_dictionary:
             response = requests.get(
-                'https://ow-api.com/v1/stats/pc/us/{}/complete'.format(overwatch_dictionary[member.name]))
+                'https://ow-api.com/v1/stats/pc/us/{}/profile'.format(overwatch_dictionary[member.name]))
             if response.ok:
                 string_response = response.json()['competitiveStats']
                 print(response.json())
                 await text_channel.send('Stats {}'.format(pprint(string_response)))
             else:
-                await  text_channel.send("Couldn't get stats for user Battle.net user '{}'. Response {}".format(
+                await text_channel.send("Couldn't get stats for user Battle.net user '{}'. Response {}".format(
                     overwatch_dictionary[member.name], response))
 
 
