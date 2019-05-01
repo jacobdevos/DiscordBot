d9d0bd9ffd0f7d3a2030a98dcdfd2e48d52fb311
Jacob de Vos <jacobdevos@hotmail.com>
2019-05-01 12:22:06 -0400

    "adding basic bot for testing"

---
+++ DiscordBot.py
@@ -0,0 +1,26 @@
+import os
+
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+def get_token():
+    return os.environ.get('APITOKEN', 'none')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(get_token())
