ddd7f591ae8c2ef8b3e8eed7f2c875dc5d2a212c
Jacob de Vos <jacobwdv@ca.ibm.com>
2019-05-02 15:17:25 -0400

    changing insert into upsert

--- DiscordBot.py
+++ DiscordBot.py
@@ -33,7 +33,7 @@ async def on_message(message):
             discord_user_key = message.author.name
             battlenet_id_value = msgs[1]
             overwatch_dictionary[discord_user_key] = battlenet_id_value
-            document = {discord_user_key: battlenet_id_value}
+            document = {"discordName": discord_user_key, "overwatchUser": battlenet_id_value}
             if not storage.find_one(document):
                 storage.insert_one(document)
             await message.channel.send('Registration complete.')
@@ -86,6 +86,6 @@ def get_battle_net_ids(discordName, table):
     print("count = {}", ids.count())
     for id in ids:
         print(id)
-    return ids;
+    return ids
 
 client.run(get_token())
