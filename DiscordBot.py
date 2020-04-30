ff472ff2eefe0b635c4b4366c269a0451da70bfa
Jacob de Vos <jacobdevos@hotmail.com>
2020-04-30 11:44:05 -0400

    fixing registration

--- DiscordBot.py
+++ DiscordBot.py
@@ -171,7 +171,8 @@ def get_random_dict_values(dict_of_dicts, num_of_values):
     random_values = []
     random_stat_tuples = []
     for i in range(0, num_of_values):
-        random_stat_tuples[i] = get_random_stat(dict_of_dicts)
+        value = get_random_stat(dict_of_dicts)
+        random_stat_tuples.append(value)
     print('random stat tuples = {}'.format(random_stat_tuples))
     for tuple in random_stat_tuples:
         random_values.append("{}: {}".format(str(tuple[0]), str(tuple[1])))
