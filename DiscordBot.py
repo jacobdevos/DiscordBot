8a7fe532d9e6a2b608d5badfda7a9f882d55b042
Jacob de Vos <jacobdevos@hotmail.com>
2020-05-02 20:30:49 -0400

    un camel casing keys.

--- DiscordBot.py
+++ DiscordBot.py
@@ -169,6 +169,7 @@ def get_embedded_stats(stats, stats_uri):
         win_percentage = stats["competitiveStats"]["careerStats"][top_hero]["game"]["winPercentage"]
         games_played = stats["competitiveStats"]["careerStats"][top_hero]["game"]["gamesPlayed"]
         random_stats = get_random_dict_values(hero_stats_dict, 4, filter_keys=["winPercentage", "gamesPlayed"])
+        
         list_of_str_fmt_stats = []
         for random_stat in random_stats.keys():
             list_of_str_fmt_stats.append(
